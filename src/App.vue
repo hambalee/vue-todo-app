14d8d71e45446be50022d00023d45eccf3703b9b
hambalee <hambalee@outlook.com>
2022-10-29 11:27:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const todoValue = ref("eating");
+
+const todoList = ref(["travel", "coding", "running"]);
+
+const onAddTodo = (newTodo: string) => {
+  todoList.value.push(newTodo);
+};
+
+const onRemoveTodo = (index: number) => {
+  // todoList.value.splice(index, 1);
+  const newTodoList = todoList.value.filter((val, i) => i != index);
+  todoList.value = [...newTodoList];
+};
+</script>
+
+<template>
+  <h1 class="title">Todo app</h1>
+  <div class="todo-wrapper">
+    <input v-model="todoValue" class="my-input" type="text" />
+  </div>
+  <div class="todo-wrapper">
+    <ul>
+      <li v-for="(todo, index) of todoList" :key="index">
+        {{ todo }}
+
+        <button @click="onRemoveTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
+
+  <div class="todo-wrapper">
+    <button @click="onAddTodo(todoValue)">Add Todo</button>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.my-input,
+button {
+  font-size: 24px;
+}
+
+.todo-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+ul > li {
+  font-size: 24px;
+}
+</style>
---
+++ src/BaseCount.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const name = ref("Lee");
+const count = ref(0);
+
+const onIncrement = () => {
+  count.value++;
+};
+
+const onDecrement = () => {
+  count.value--;
+};
+
+const countText = computed(() => `Count: ${count.value}`);
+</script>
+
+<template>
+  <h1>My name is {{ name }}.</h1>
+  <div class="count-text">{{ countText }}</div>
+  <!-- Directive -->
+  <div v-if="count % 2 == 1" class="count-text">is odd</div>
+  <div v-else-if="count == 0" class="count-text">is zero</div>
+  <div v-else class="count-text">is even</div>
+  <button class="count-button" @click="onIncrement()">+</button>
+  <button class="count-button" @click="onDecrement()">-</button>
+</template>
+
+<style scoped>
+.count-button {
+  padding: 16px;
+  margin: 16px;
+}
+
+.count-text {
+  font-size: 32px;
+}
+</style>
